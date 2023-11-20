4250d151ecfaf9016cc063c57f50801a3423408b
vladik <121016003+vladikv@users.noreply.github.com>
2023-11-21 01:47:19 +0200

    Work with basket. Add apples

--- ApplePicker/Assets/Scripts/ApplePicker.cs
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketButtomY = -14f;
+    public float basketSpacingY = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketButtomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition; 
+
+        mousePos2D.z = -Camera.main.transform.position.z; 
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); 
+
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    { //a
+      // ќтыскать €блоко, попавшее в эту корзину
+        GameObject collidedWith = coll.gameObject; // b
+        if (collidedWith.tag == "Apple" ) { 
+            Destroy(collidedWith);
+        }
     }
 }
+
--- ApplePicker/Assets/Scripts/AppleTree.cs
+++ ApplePicker/Assets/Scripts/AppleTree.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //
+    public GameObject applePrefab;
+    //
+    public float speed = 1f;
+    //
+    public float leftAndRightEdge = 10f;
+    //
+    public float chanceToChangeDirections = 0.1f;
+    //
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DropApple", 2);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = this.transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
