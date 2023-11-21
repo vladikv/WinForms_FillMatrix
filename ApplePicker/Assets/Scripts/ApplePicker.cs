6587ae88c7d42e988da25061e080b3a3296ba49b
vladik <121016003+vladikv@users.noreply.github.com>
2023-11-21 02:01:21 +0200

    Edit basket

--- ApplePicker/Assets/Scripts/ApplePicker.cs
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -20,29 +20,4 @@ public class ApplePicker : MonoBehaviour
             tBasketGO.transform.position = pos;
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 mousePos2D = Input.mousePosition; 
-
-        mousePos2D.z = -Camera.main.transform.position.z; 
-
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); 
-
-        Vector3 pos = this.transform.position;
-        pos.x = mousePos3D.x;
-        this.transform.position = pos;
-
-    }
-
-    void OnCollisionEnter(Collision coll)
-    { //a
-      // ќтыскать €блоко, попавшее в эту корзину
-        GameObject collidedWith = coll.gameObject; // b
-        if (collidedWith.tag == "Apple" ) { 
-            Destroy(collidedWith);
-        }
-    }
 }
-
